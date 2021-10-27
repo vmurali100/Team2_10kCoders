880f83c4281caf202bea69547f1359ba2ec1bfb8
9srikanth <satyasrikanthnidadavolu999@gmail.com>
2021-10-27 14:50:51 +0530

    some

---
+++ oct 19&20work/basic-88/styles/data.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a;
+    float b;
+    char c;
+    a=10;
+    b=0.5;
+    c="d"
+    printf("the giveen integer is %d",a);
+    printf("the given float is%f",b);
+    printf("the given char is%c",c);
+    getch();
+}    
