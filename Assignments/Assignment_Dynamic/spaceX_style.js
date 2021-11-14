07f2ba25adc559f9c9c4fea899fc2900e161c906
PunemHarish <punemharish71@gmail.com>
2021-11-14 14:22:52 +0530

    Dynamic templates

--- Assignments/Assignment_Dynamic/spaceX_style.js
+++ Assignments/Assignment_Dynamic/spaceX_style.js
@@ -14,10 +14,16 @@ document.getElementById("hgroup").appendChild(h2)
 // ============================ NAVIGATION BAR ====================
 
 var spaceDetails= ["Falcon", "Dragon", "Starship", "Rideshare", "Human Spacelight"]
-for (i = o; i < spaceDetails.length; i++) {
-    var list = document.createElement("li");
-    // var anchor = document.createElement("a");
-    // anchor.innerHTML = nav[i]
-    // li.appendChild(anchor)
+for(i=0;i<spaceDetails.length;i++){
+    var list = document.createElement("li");                  //step - 1
+    var anchor = document.createElement("a");
+    anchor.innerHTML = spaceDetails[i]                        //step - 2
+    list.appendChild(anchor)                                  //step - 3
     document.getElementById("demo").appendChild(list)
 }
+
+// =============================== BANNER ============================
+
+document.getElementById("myBanner").setAttribute("src","https://freight.cargo.site/t/original/i/d7d59958ac7c19813acf8b0f2ad4dbf1329c602086870e9faa3d380acbc12481/frame4_1.jpg")
+
+
