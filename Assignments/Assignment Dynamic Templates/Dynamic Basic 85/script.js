97c8b1332975b2619f3861621821a68da20b26fe
ERVIN-PRABHU <ervinprabhu5@gmail.com>
2021-11-15 01:50:18 +0530

    template 3

--- Assignments/Assignment Dynamic Templates/Dynamic Basic 85/script.js
+++ Assignments/Assignment Dynamic Templates/Dynamic Basic 85/script.js
@@ -37,6 +37,9 @@ document.getElementById("section1").appendChild(sec1_p);
 //Container section2 images
 var sec2_article = document.createElement("article");
 var sec2_figure = document.createElement("figure");
+var sec2_figcaption = document.createElement("figcaption");
+var sec2_a =document.createElement("a");
+var sec2_p = document.createElement("p");
 var sec2_ul = document.createElement("ul");
 for(i=0; i<4; i++){
     var sec2_li = document.createElement("li");
@@ -48,82 +51,61 @@ for(i=0; i<4; i++){
 
     sec2_ul.appendChild(sec2_li);
 }
+sec2_a.innerHTML = "View all our recent works for ipl here »";
+sec2_a.setAttribute("href","https://www.iplt20.com/");
+sec2_a.setAttribute("target","_blank");
+
 sec2_figure.appendChild(sec2_ul);
-sec2_article.appendChild(sec2_figure);
+sec2_p.appendChild(sec2_a);
+sec2_figcaption.appendChild(sec2_p);
+sec2_article.append(sec2_figure, sec2_figcaption);
 document.getElementById("section2").appendChild(sec2_article);
 
 
-// //Container Homepage section3 h1
-// var sec3_h1 = document.createElement("h1");
-// sec3_h1.innerHTML="Forms of Cricket";
-// document.querySelector(".homepage").insertBefore(sec3_h1, document.querySelector(".section3"));
+//Container Homepage section3 article 
+var sec3_strong_array =["CSK","MI","RCB","SRH","DC","PBKS","RR","KKR"]
+var sec3_images_alt_array = ["Chennai_Super_Kings","Mumbai_Indians","Royal_Challengers_Bangalore","Sunrisers_Hyderabad","Delhi_Capitals","Punjab_Kings","Rajasthan_Royals","Kolkata_Knight_Riders"]
 
-// //Container Homepage section3 article 
-// var sec3_images_alt_array = ["Test_Cricket","One_Day_International","Twenty20"]
+var sec3_para_array = ["The Chennai Super Kings (CSK) is a franchise cricket team based in Chennai, Tamil Nadu. Founded in 2008",
+"The Mumbai Indians (MI) are a franchise cricket team based in Mumbai, Maharashtra. Founded in 2008.",
+"The Royal Challengers Bangalore (RCB) are a franchise cricket team based in Bangalore, Karnataka. It was founded in 2008.",
+"The Sunrisers Hyderabad (SRH) are a franchise cricket team based in Hyderabad, Telangana, India. Founded in 2012.",
+"The Delhi Capitals (DC) are a franchise cricket team based out of Delhi in the Indian Premier League (IPL). Founded in 2008 .",
+"The Punjab Kings (PBKS) are a franchise cricket team based in Mohali, Punjab India. Established in 2008.",
+"The Rajasthan Royals (RR) are a franchise cricket team based in Jaipur, Rajasthan. Founded in 2008.",
+"The Kolkata Knight Riders (KKR) are a franchise cricket team representing the city of Kolkata in the Indian Premier League."
+]
+for(i=0; i<(sec3_para_array).length; i++)
+{   
+    var sec3_image = document.createElement("img");
+    var sec3_figure = document.createElement("figure");
+    var sec3_strong = document.createElement("strong");
+    var sec3_para = document.createElement("p");
+    var sec3_readmore_a = document.createElement("a");
+    var sec3_readmore_div = document.createElement("div");
+    var sec3_article = document.createElement("article");
 
-// var sec3_para_array = ["Test cricket is the form of the sport of cricket with the longest match duration and is considered the game's highest standard.",
-// "A ODI is a form of limited overs cricket with a fixed number of overs, currently 50, with the game lasting up to 9 hours.",
-// "T20 is a shortened game format of cricket with restricted to a maximum of 20 overs."
-// ]
-// for(i=0; i<(sec3_para_array).length; i++)
-// {   
-//     var sec3_image = document.createElement("img");
-//     var sec3_figure = document.createElement("figure");
-//     var sec3_strong = document.createElement("strong");
-//     var sec3_para = document.createElement("p");
-//     var sec3_readmore_a = document.createElement("a");
-//     var sec3_readmore_div = document.createElement("div");
-//     var sec3_article = document.createElement("article");
+    sec3_strong.innerHTML = sec3_strong_array[i];
+    sec3_para.innerHTML = sec3_para_array[i];
+    sec3_readmore_a.innerHTML = "Readmore »";
 
-//     sec3_strong.innerHTML = sec3_images_alt_array[i];
-//     sec3_para.innerHTML = sec3_para_array[i];
-//     sec3_readmore_a.innerHTML = "Readmore »";
+    sec3_image.setAttribute("src","./images/demo/32x32"+("("+(i+1)+")"));
+    sec3_image.setAttribute("alt",sec3_images_alt_array[i]);
+    sec3_readmore_a.setAttribute("href","https://en.wikipedia.org/wiki/"+sec3_images_alt_array[i]);
+    sec3_readmore_div.setAttribute("class","readmore");
 
-//     sec3_image.setAttribute("src","./images/demo/32x32"+("("+(i+1)+")"));
-//     sec3_image.setAttribute("alt",sec3_images_alt_array[i]);
-//     sec3_readmore_a.setAttribute("href","https://en.wikipedia.org/wiki/"+sec3_images_alt_array[i]);
-//     sec3_readmore_div.setAttribute("class","readmore");
+    sec3_figure.appendChild(sec3_image);
+    sec3_readmore_div.appendChild(sec3_readmore_a);
+    sec3_article.append(sec3_figure, sec3_strong, sec3_para, sec3_readmore_div);
 
-//     sec3_figure.appendChild(sec3_image);
-//     sec3_readmore_div.appendChild(sec3_readmore_a);
-//     sec3_article.append(sec3_figure, sec3_strong, sec3_para, sec3_readmore_div);
+    document.getElementsByClassName("section3")[0].appendChild(sec3_article);
+}
 
-//     document.getElementsByClassName("section3")[0].appendChild(sec3_article);
-// }
+//Footer
+var footer_p1 = document.createElement("p");
+var footer_p2 = document.createElement("p");
 
-// // Container Homepage section4 h1
-// var sec4_h1 = document.createElement("h1");
-// sec4_h1.innerHTML="Latest from the gallery";
-// document.querySelector(".homepage").insertBefore(sec4_h1, document.querySelector(".section4"));
+footer_p1.innerHTML = "Copyright © 2021 - All Rights Reserved - IPL";
+footer_p2.innerHTML = "Template cloned from OS-Templates by Ervin";
 
-// //Container Homepage section4 aticle
-// var sec4_figcap_array = ["smriti mandana", "Mitali Raj", "MS Dhoni", "Virat Kohli"]
-// for(i=0; i<4; i++)
-// {
-//     var sec4_article = document.createElement("article");
-//     var sec4_figure = document.createElement("figure");
-//     var sec4_image = document.createElement("img");
-//     var sec4_figcap = document.createElement("figcaption");
-
-//     sec4_image.setAttribute("src","images/demo/215x315"+"("+(i+1)+")");
-//     sec4_figcap.innerHTML = sec4_figcap_array[i];
-
-//     sec4_figure.append(sec4_image, sec4_figcap);
-
-//     sec4_article.appendChild(sec4_figure);
-
-//     document.querySelector(".section4").appendChild(sec4_article);
-// }
-
-
-
-
-
-// //Footer
-// var footer_p1 = document.createElement("p");
-// var footer_p2 = document.createElement("p");
-
-// footer_p1.innerHTML = "Copyright © 2021 - All Rights Reserved - ScoreBuddy";
-// footer_p2.innerHTML = "Template cloned from OS-Templates by Ervin";
-
-// document.getElementById("footer").append(footer_p1, footer_p2);
+document.getElementById("footer").append(footer_p1, footer_p2);
