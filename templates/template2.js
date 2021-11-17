5b53fd8af6c6d82bf72d0b1c040da4a355beda8f
tdp143 <tejdeepchowdary143@gmail.com>
2021-11-17 21:51:04 +0530

    new

---
+++ templates/template2.js
@@ -0,0 +1,11 @@
+
+var movieDetails=["tel-comics","tel-movie","tel-director","tel-producers","tel-actors"]
+for(i=0;i<movieDetails.length;i++){
+    //step1:
+   var li= document.createElement("li")
+   
+
+
+document.getElementById("demo").appendChild("li")
+
+}
