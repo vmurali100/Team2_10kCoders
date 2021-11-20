1c48053f7a1d3a4dd18083541ecb5623d5ee667e
teja6392 <tejastudent777@gmail.com>
2021-11-20 10:06:21 +0530

    second Try for Entire forms th Edit&Delete

--- Novomber 19 2021/table.js
+++ Novomber 19 2021/table.js
@@ -8,6 +8,52 @@ function table(){
             td.innerText=person1[a]
             tr.appendChild(td)
         }
+        var editTd = document.createElement("td")
+        tr.appendChild(editTd)
+        editBTn= document.createElement("button")
+        editBTn.innerText="Edit"
+        editBTn.setAttribute("onclick","edit("+ i +")")
+        editBTn.setAttribute("class","btn btn-warning")
+        editTd.appendChild(editBTn)
+
+        var delTd = document.createElement("td")
+        tr.appendChild(delTd)
+        delBTn= document.createElement("button")
+        delBTn.innerText="Delete"
+        delBTn.setAttribute("onclick","del("+ i +")")
+        delBTn.setAttribute("class","btn btn-danger")
+        delTd.appendChild(delBTn)
+
     })
 }
+//To Display Table Data That Was stored in local storage
 table()
+
+var index;
+function edit(i){
+    index=i;
+    for(a in person){
+        document.getElementById(a).value = persons[i][a]
+        document.getElementById("update").style.display = "block"
+        document.getElementById("submit").style.display = "none"
+    }
+}
+
+function updatePerson(){
+    for(a in person){
+        person[a] = document.getElementById(a).value
+    }
+    persons[index] = {...person};
+    table()
+    localStorage.setItem('persons',JSON.stringify(persons))
+    document.getElementById("update").style.display = "none"
+    document.getElementById("submit").style.display = "block"
+    clear()
+}
+
+function del(i){
+    persons.splice(i,1)
+    //console.log("Deleting in process")
+    localStorage.setItem("persons",JSON.stringify(persons))
+    table()
+}
