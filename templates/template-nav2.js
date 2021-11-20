b0489912fc9db72f9b569b0843a8a3a73bdde704
tdp143 <tejdeepchowdary143@gmail.com>
2021-11-20 13:46:22 +0530

    n

--- templates/template-nav2.js
+++ templates/template-nav2.js
@@ -1,5 +1,5 @@
 
-var movieDetails=["tel-comics","tel-movie","tel-director","tel-producers","tel-actors"]
+/*var movieDetails=["tel-comics","tel-movie","tel-director","tel-producers","tel-actors"]
 for(i=0;i<movieDetails.length;i++){
     var list=document.createElement("li")
     var anchor=document.createElement("a")
@@ -10,4 +10,12 @@ for(i=0;i<movieDetails.length;i++){
     list.appendChild(anchor)
       document.getElementById("demo").appendChild(list)
       console.log(list)
+}*/
+var movieDetails=["tel-comics","tel-movie","tel-director","tel-producers","tel-actors"]
+for(i=0;i=movieDetails.length;i++){
+  //step1: create an element
+  var links = document.createElement("li")
+  //step2: adding text to an element
+  //step3: apphendchild to an element
+  document.getElementById("demo").appendChild(links)
 }
