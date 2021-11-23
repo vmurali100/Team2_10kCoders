b381a9126bedd6803a2c465fe5ff1bcffd86a4c3
ERVIN-PRABHU <ervinprabhu5@gmail.com>
2021-11-24 03:55:51 +0530

    8th api added

--- Assignments/Assignment CRUD API's/Json_placeholder_API's/API5/display.js
+++ Assignments/Assignment CRUD API's/Json_placeholder_API's/API5/display.js
@@ -1,11 +1,7 @@
 user = {
-    fname : "",
-    lname : "",
-    tel : "",
-    address:"",
-    city: "",
-    state: "",
-    zip: ""
+    userId : "",
+    title : "",
+    completed : ""
 }
 function display(data)
 { 
@@ -21,9 +17,12 @@ function display(data)
         slno.innerHTML = i+1;
         tr.appendChild(slno);
         for(a in user){
-            var td = document.createElement("td");
-            td.innerHTML = user[a];
-            tr.appendChild(td);
+            if(a !== "id")
+            {
+                var td = document.createElement("td");
+                td.innerHTML = user[a];
+                tr.appendChild(td);
+            }
         }
 
         var edittd = document.createElement("td");
