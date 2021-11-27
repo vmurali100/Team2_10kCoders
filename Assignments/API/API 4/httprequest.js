f47401766944fc2c1e837d50ce44eb7e43b62eb3
PunemHarish <punemharish71@gmail.com>
2021-11-27 08:47:42 +0530

    adding

---
+++ Assignments/API/API 4/httprequest.js
@@ -0,0 +1,15 @@
+function getData()
+{
+    document.getElementById("getData").style.display = "none";
+
+    var getInfo = new XMLHttpRequest();
+    getInfo.open("GET","http://www.filltext.com/?rows=10&id={index}&email={email}&username={username}&password={randomString|5}&pretty=true")
+    getInfo.send();
+    getInfo.onreadystatechange = function(){
+        if(getInfo.status == 200 && getInfo.readyState == 4)
+        {
+            var data = JSON.parse(getInfo.response);
+            display(data);    
+        }
+    }
+}
---
+++ Assignments/API/API 5/display.js
@@ -0,0 +1,44 @@
+ser = {
+    id : "",
+    email : "",
+    username : "",
+    password : ""
+}
+function display(data)
+{ 
+    if(data !== undefined)
+    {
+        users = data;
+    }
+    document.getElementById("thead").style.display = "table-header-group"
+    document.querySelector("tbody").innerHTML = "";
+    users.forEach((user,i)=>{
+        var tr = document.createElement("tr");
+        var slno = document.createElement("td");
+        slno.innerHTML = i+1;
+        tr.appendChild(slno);
+        for(a in user){
+            var td = document.createElement("td");
+            td.innerHTML = user[a];
+            tr.appendChild(td);
+        }
+
+        var edittd = document.createElement("td");
+        var editbtn = document.createElement("button");
+        editbtn.innerHTML = "Edit";
+        editbtn.setAttribute("class","btn btn-warning");
+        editbtn.setAttribute("onclick","edituser("+i+")");
+        edittd.appendChild(editbtn);
+        tr.appendChild(edittd);
+
+        var deltd = document.createElement("td");
+        var delbtn = document.createElement("button");
+        delbtn.innerHTML = "Delete"
+        delbtn.setAttribute("class","btn btn-danger");
+        delbtn.setAttribute("onclick","deluser("+i+")");
+        deltd.appendChild(delbtn);
+        tr.appendChild(deltd);
+
+        document.querySelector("tbody").appendChild(tr);
+    })
+}
