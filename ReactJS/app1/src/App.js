d14411f69798136c96125bc135f4bbb5e8d84bae
ERVIN-PRABHU <ervinprabhu5@gmail.com>
2021-12-14 09:30:13 +0530

    react projects

---
+++ ReactJS/app1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Ervin from './Ervin';
+
+function App() {
+  return (
+    <div className="App">
+      <Ervin/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/app1/src/Ervin.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Ervin()
+{
+    return <h2>I am from ervin component</h2>
+}
