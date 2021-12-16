55ad58d74c10ff85bbdcd76d3bb640f9e3d15bb2
ERVIN-PRABHU <ervinprabhu5@gmail.com>
2021-12-16 08:04:42 +0530

    added

--- ReactJS/app1/src/App.js
+++ ReactJS/app1/src/App.js
@@ -1,19 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Component1 from './Component1';
-import Component2 from './Component2';
-import Component3 from './Component3';
-import Component5 from './Component5';
-import Component4 from './Component4';
+import { One } from './Component1';
 
 function App() {
   return (
     <div className="App">
-      <Component1/>
-      <Component2/>
-      <Component3/>
-      <Component4/>
-      <Component5/>
+    <One/>
     </div>
   );
 }
--- ReactJS/app1/src/Component1.js
+++ ReactJS/app1/src/Component1.js
@@ -1,6 +1,13 @@
-import React from "react";
-
-export default function Component1()
+// export function Component1()
+// {
+//     return <h1>This is Co</h1>
+// }
+export function One()
 {
-    return <h1>This is from app1 Component1 </h1>
+    return <h1>This is from app1 Component1 function1 </h1>
+}
+
+export function Two()
+{
+    return <h1>This is form app1 Component1 function2</h1>
 }
