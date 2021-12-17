4239032cf0f323b29dc20375cc0a2fe10a64e41d
vmurali100 <vmurali100@gmail.com>
2021-12-17 09:22:44 +0530

    Adding ReactJS Forms

--- ReactJS/app2/src/User.jsx
+++ ReactJS/app2/src/User.jsx
@@ -1,21 +1,69 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
+import './index.css';
 
 export const User = () => {
-    const [fname, setfname] = useState("");
-    const handleInput =(e)=>{
-        console.log(e.target.value)
-        setfname(e.target.value)
-    }
-    return (
-        <div>
-            <form>
-            <label htmlFor="fname">First Name : </label>
-            <input type="text" 
-                name="fname"  
-                value={fname} 
-                onChange={(e)=>{handleInput(e)}}/>
-            <button>Get User</button>
-        </form>            
-        </div>
-    )
-}
+  const [user, setuser] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+  });
+  const handleInput = (e) => {
+    // setfname(e.target.value);
+    let newUser = { ...user };
+    let nameofInput = e.target.name;
+    newUser[nameofInput] = e.target.value;
+    setuser(newUser);
+  };
+
+  const getUser = () => {
+    console.log(user);
+    clearForm();
+  };
+
+  const clearForm = () => {
+    setuser({
+      fname: "",
+      lname: "",
+      email:""
+    });
+  };
+  return (
+    <div id="myForm">
+      <form>
+        <label htmlFor="fname">First Name : </label>
+        <input
+          type="text"
+          name="fname"
+          value={user.fname}
+          onChange={(e) => {
+            handleInput(e);
+          }}
+        />{" "}
+        <br />
+        <label htmlFor="fname">Last Name : </label>
+        <input
+          type="text"
+          name="lname"
+          value={user.lname}
+          onChange={(e) => {
+            handleInput(e);
+          }}
+        />{" "}
+        <br />
+        <label htmlFor="fname">Email : </label>
+        <input
+          type="text"
+          name="email"
+          value={user.email}
+          onChange={(e) => {
+            handleInput(e);
+          }}
+        />{" "}
+        <br />
+        <button type="button" onClick={getUser}>
+          Get User
+        </button>
+      </form>
+    </div>
+  );
+};
