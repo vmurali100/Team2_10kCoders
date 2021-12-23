e4bd0bd6e619b7f68d36b870f7f2c0b59f246fd3
ERVIN-PRABHU <ervinprabhu5@gmail.com>
2021-12-23 17:21:52 +0530

    added first function component api placeholder comments

--- ReactJS/app4/src/App.js
+++ ReactJS/app4/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Placeholder1 from "./Placeholder1"
 
 function App() {
   return (
     <div className="App">
-      <Placeholder1/>
+      <Placeholder1 />
     </div>
   );
 }
--- ReactJS/app4/src/Placeholder1.jsx
+++ ReactJS/app4/src/Placeholder1.jsx
@@ -3,15 +3,41 @@ import axios from "axios";
 
 function Placeholder1() {
   const url = "http://jsonplaceholder.typicode.com/comments";
-  const [comments, setComments] = useState();
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    axios.get(url).then(({ data }) => {});
+    axios.get(url).then(({ data }) => {
+        setComments(data)
+    });
   }, []);
   return (
     <div>
-      <h1>Placeholder component</h1>
-      <ul>{comments.map((comment) => {})}</ul>
+        <h1>Placeholder comments</h1>
+      <div className="table-responsive ms-5 me-5 m-auto">
+      <table className="table">
+        <thead>
+            <th scope="col">PostId</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Body</th>
+            
+        </thead>
+        <tbody>
+        {
+            comments.map((comment)=>
+            {
+                console.log(comment)
+                return (<tr key={comment.id}>
+                    <td>{comment.postId}</td>
+                    <td>{comment.name}</td>
+                    <td>{comment.email}</td>
+                    <td>{comment.body}</td>
+                    </tr>); 
+            })
+        }
+        </tbody>
+      </table>
+      </div>
     </div>
   );
 }
