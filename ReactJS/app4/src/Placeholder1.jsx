8a576ca467afda717fdb0a81edce87f4b6fa2e32
ERVIN-PRABHU <ervinprabhu5@gmail.com>
2021-12-23 10:59:01 +0530

    ...

---
+++ ReactJS/app4/src/Placeholder1.jsx
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+
+function Placeholder1()
+{
+    const url = "http://jsonplaceholder.typicode.com/comments"
+    const [product, setProduct] = useState();
+
+    useEffect(()=>{
+        console.log(data)
+    }).then({data})
+    return <div>
+    <h1>Placeholder component</h1>
+    </div>
+}
+export default Placeholder1;
