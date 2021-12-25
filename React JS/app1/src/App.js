0f5d3835081730157be459bcdc1c9f6273412ef9
PunemHarish <punemharish71@gmail.com>
2021-12-25 10:17:22 +0530

    authentication api

--- React JS/app1/src/App.js
+++ React JS/app1/src/App.js
@@ -5,16 +5,13 @@ import { Cart } from './APIs/Functional Component/Fake store/Cart';
 import { Products } from './APIs/Functional Component/Fake store/Products';
 import { Name } from './APIs/Functional Component/Fill Text/Name';
 import { Users } from './APIs/Functional Component/Json/Users';
-import { Albums } from './APIs/Functional Component/Json/Albums';
-import { Comments } from './APIs/Functional Component/Json/Comments';
-import { Photos } from './APIs/Functional Component/Json/Photos';
-import { Posts } from './APIs/Functional Component/Json/Posts';
-import { Todos } from './APIs/Functional Component/Json/Todos';
+import { Cricket } from './Cricket';
+
 
 function App() {
   return (
     <div className="App">
-      <Users/>
+      <Cricket/>
     </div>
   );
 }
