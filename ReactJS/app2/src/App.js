2528338fad6640540a5912f32471ce49426aed49
9srikanth <satyasrikanthnidadavolu999@gmail.com>
2021-12-26 10:26:02 +0530

    adding

--- ReactJS/app2/src/App.js
+++ ReactJS/app2/src/App.js
@@ -4,13 +4,14 @@ import  { Krishna, Rambabu, Shyam }  from './Pallavi';
 import { Keyboard, Mouse, Rexon, Space } from './Laptop';
 import { Bussiness } from './Business';
 import { DetailsForm } from './DetailsForm';
+import { Cricket } from './cricket';
 
 
 function App() {
   return (
     <div className="App">
       <h2>I am app.js</h2>
-      <DetailsForm/>
+      <Cricket/>
      </div>
     
   );
--- ReactJS/app2/src/Cricket.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function Cricket(){
-    return <h1>Cricket</h1>
-}
