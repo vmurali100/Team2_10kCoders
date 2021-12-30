7477c2033f6106a18e7d801a948576471e99c2e7
tdp143 <tejdeepchowdary143@gmail.com>
2021-12-30 17:32:54 +0530

    react

---
+++ react-js/app5/src/Form.jsx
@@ -0,0 +1,12 @@
+export const Input = () => {
+    const [fname, setfname] = useState("")
+    
+}
+return (
+    <div>
+        <form >
+            <label html="fname" >Firstname</label>
+            <input type="text" />
+    </form>
+    </div>
+)
