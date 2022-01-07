ec54eaa4bead8557eb67fcda62daf8c6a2db6bae
PunemHarish <punemharish71@gmail.com>
2022-01-07 09:24:43 +0530

    completed All the API's

--- React JS/app1/src/index.js
+++ React JS/app1/src/index.js
@@ -12,13 +12,11 @@ import EmpReducer from './Redux API\'s/Fill Text/Employee/Reducer';
 import PersonReducer from './Redux API\'s/Fill Text/Person/Reducer';
 import CartReducer from './Redux API\'s/Fake store/Cart/Reducer';
 import ProductReducer from './Redux API\'s/Fake store/Products/Reducer/Index';
-import UserReducer from './Redux API\'s/Fake store/Users/Reducer';
 import AlbumReducer from './Redux API\'s/Json/Albums/Reducer';
 import CommentReducer from './Redux API\'s/Json/Comments/Reducer';
 import PhotoReducer from './Redux API\'s/Json/Photos/Reducer';
 import PostReducer from './Redux API\'s/Json/Posts/Reducer';
 import TodoReducer from './Redux API\'s/Json/Todos/Reducer';
-import UsersReducer from './Redux API\'s/Json/Users/Reducer';
 
 const store = createStore(TodoReducer)
 console.log(store)
