db17f4ebb907f84a73fd02d54987d3b2f326fe31
akhilankathi <akhilankathi@gmail.com>
2022-01-07 12:22:23 +0530

    adding added

--- ReactJS/FAKESTORE/products/src/index.js
+++ ReactJS/FAKESTORE/products/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {createStore} from "redux"
-import reportWebVitals from './reportWebVitals';
-import Reducer from './Redux/Reducer';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import reportWebVitals from "./reportWebVitals";
+import Reducer from "./Redux/Reducer";
+import { Provider } from "react-redux";
 
-
-const store = createStore(Reducer)
+const store = createStore(Reducer);
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-  <App />
-  </Provider>
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
