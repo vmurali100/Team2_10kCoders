b1751b3f7b8bd671865eae6f225edeb5606fb4c8
PunemHarish <punemharish71@gmail.com>
2022-01-21 14:58:11 +0530

    Completed React Router By using API'S...,

---
+++ React JS/api_router/src/Components/json/Photos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useState,useEffect} from 'react' 
+import axios from 'axios'
+
+
+export const Photos = () => {
+  const[data,setData]=useState([]);
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/albums').then(
+      response=>setData(response.data)
+    )
+
+  },[])
+  return <div>
+      <h2 style={{color:'red'}}>Json Place Holder Photos Component</h2>
+      {data.map(item => <p key={item.title} style={{color:'black'}}><span style={{fontSize:'20px', fontWeight:'bold'}}>Title:</span> {item.title}</p>)}
+
+  </div>;
+};
