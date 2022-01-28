7d305c474300e5e9dfb0ba1865a9022850cc0b1d
teja6392 <tejastudent777@gmail.com>
2022-01-28 23:44:52 +0530

    Project 1 Completed

--- PRoject1/distapp/src/App.js
+++ PRoject1/distapp/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { DistrictDetails } from './DistrictDetails';
+import { Login } from './Components/Login';
+import { Register } from './Components/Register';
+import { Routes, Route, Link } from "react-router-dom"
+import React,{useEffect} from 'react';
+import { NavComp } from './Components/NavComp';
 
 function App() {
+
   return (
     <div>
-      <nav className="py-2 bg-light border-bottom">
-    <div className="container d-flex flex-wrap">
-      <ul className="nav me-auto">
-      
-      </ul>
-      <ul className="nav">
-        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Login</a></li>
-        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Sign up</a></li>
-      </ul>
-    </div>
-  </nav>
-  <DistrictDetails />
+     <NavComp />
+      <div className='container'>
+        <Routes>
+          <Route path="" element={<DistrictDetails />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </div>
     </div>
   );
 }
