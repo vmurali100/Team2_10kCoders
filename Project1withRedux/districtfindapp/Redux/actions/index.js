7cd82d65e7f4c021ef25cbbfdeda84417894595d
teja6392 <tejastudent777@gmail.com>
2022-02-05 12:14:27 +0530

    New Project Done

---
+++ Project1withRedux/districtfindapp/Redux/actions/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { GET_ALL_USERS } from "./actionTypes"
+export const getAllUsers = (users)=>{
+    return {
+        type:GET_ALL_USERS,paylaod:users
+    }
+}
+export const addUser = ()=>{
+    return state
+}
+
+export const editUSer = ()=>{
+    return state
+}
+
+export const deleteUser = ()=>{
+    return state
+}
